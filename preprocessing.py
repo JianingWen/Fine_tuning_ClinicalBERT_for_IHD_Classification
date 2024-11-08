e52b7f6e485319b4607e5500f10182fce2a3f70a
Calvin York <109571293+CalvinYorkCS@users.noreply.github.com>
2024-11-08 15:17:17 -0600

    Added text preprocessing preprocessing.py
    Adapted text preprocessing from https://github.com/kexinhuang12345/clinicalBERT/blob/master/preprocess.py
    
    - Creates a dataframe of the clinical notes, the labels, and the subject id
    - splits each text into 318 word chunks, which is roughly how much clinicalBERT processes
    - performs more preprocessing on the text like converting all text to lowercase, replacing new lines with spaces, replacing "dr." with "doctor", filling in missing values, etc.

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import re
+from tqdm import tqdm
 
 df_notes = pd.read_csv("notes.csv")
 df_notes = df_notes.sort_values(by=['SUBJECT_ID', 'HADM_ID', 'CHARTDATE'])
@@ -11,3 +13,59 @@ df_icd9 = pd.read_csv('icd9.csv')
 cad_codes = ['410', '411', '412', '413', '414']
 icd9_with_cad = df_icd9.loc[df_icd9['ICD9_CODE'].str.startswith(tuple(cad_codes)), 'SUBJECT_ID']
 df_notes['HAS_CAD'] = df_notes['SUBJECT_ID'].isin(icd9_with_cad)
+
+# Preprocess text - adapted from https://github.com/kexinhuang12345/clinicalBERT/blob/master/preprocess.py 
+def preprocess1(x):
+    y = re.sub('\\[(.*?)\\]', '', x)  # remove de-identified brackets
+    y = re.sub('[0-9]+\.', '', y)  # remove 1.2. since the segmenter segments based on this
+    y = re.sub('dr\.', 'doctor', y)
+    y = re.sub('m\.d\.', 'md', y)
+    y = re.sub('admission date:', '', y)
+    y = re.sub('discharge date:', '', y)
+    y = re.sub('--|__|==', '', y)
+    return y
+
+import re
+from tqdm import tqdm
+
+def preprocess1(x):
+    y = re.sub('\\[(.*?)\\]', '', x)  # remove de-identified brackets
+    y = re.sub('[0-9]+\.', '', y)  # remove 1.2. since the segmenter segments based on this
+    y = re.sub('dr\.', 'doctor', y)
+    y = re.sub('m\.d\.', 'md', y)
+    y = re.sub('admission date:', '', y)
+    y = re.sub('discharge date:', '', y)
+    y = re.sub('--|__|==', '', y)
+    return y
+
+def preprocessing(df_notes):
+    df_notes['TEXT'] = df_notes['TEXT'].fillna(' ')
+    df_notes['TEXT'] = df_notes['TEXT'].str.replace('\n', ' ')
+    df_notes['TEXT'] = df_notes['TEXT'].str.replace('\r', ' ')
+    df_notes['TEXT'] = df_notes['TEXT'].apply(str.strip)
+    df_notes['TEXT'] = df_notes['TEXT'].str.lower()
+
+    df_notes['TEXT'] = df_notes['TEXT'].apply(lambda x: preprocess1(x))
+
+    # to get 318 words chunks for readmission tasks
+    rows = []
+    for i in tqdm(range(len(df_notes))):
+        x = df_notes.TEXT.iloc[i].split()
+        n = int(len(x) / 318)
+        for j in range(n):
+            rows.append({
+                'TEXT': ' '.join(x[j * 318:(j + 1) * 318]),
+                'Label': df_notes.HAS_CAD.iloc[i],
+                'ID': df_notes.HADM_ID.iloc[i]
+            })
+        if len(x) % 318 > 10:
+            rows.append({
+                'TEXT': ' '.join(x[-(len(x) % 318):]),
+                'Label': df_notes.HAS_CAD.iloc[i],
+                'ID': df_notes.HADM_ID.iloc[i]
+            })
+
+    want = pd.DataFrame(rows)
+    return want
+
+df_notes = preprocessing(df_notes)
